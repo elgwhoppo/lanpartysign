581484b8a08bba29f627b225354adf5e5764771c
elgwhoppo <joeman113@gmail.com>
2016-11-15 19:10:38 -0800

    Update grabping.sh

--- grabping.sh
+++ grabping.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-#grabs the ping of a server and outs it to a file
-#place in 
+# grabs the ping of a server and outs it to a file
+# on pfSense this shell script can be placed in the /usr/local/etc/rc.d/ directory for automatic start
+# make sure to run chmod +x grabbw.sh so that it can be executed. 
+# recommended minmum delay of 1s to ensure ping completes
 
 while : 
 do
