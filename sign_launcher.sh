82f6655eb7aaa444c9726d7cf3ddbf595df40687
elgwhoppo <joeman113@gmail.com>
2018-08-25 09:38:45 -0400

    Update sign_launcher.sh

--- sign_launcher.sh
+++ sign_launcher.sh
@@ -4,6 +4,9 @@
 # Scroll to the bottom and specify the full location of this script.
 # For Example: @reboot /home/pi/sign_launcher.sh
 # should be placed at the bottom of the crontab.
+# For added stability, reboot the pi every 6 hours or so, there are still weird crashes on the pi. 
+# Sudo -i to switch to root and open crotab with crontab -e
+# Add the following: 0 */6 * * * /sbin/reboot
 
 sleep 30
 screen -dmS sign sudo python /home/pi/python.py
