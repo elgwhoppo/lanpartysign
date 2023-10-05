362a5dda4dd4806ce24391f78543efda54af76c4
Joe Clarke <joeman113@gmail.com>
2023-10-05 09:07:28 -0400

    WORKING2

--- snmp.py
+++ snmp.py
@@ -148,39 +148,8 @@ def handle_error(pipe, message):
     if pipe:
         pipe.send("UHH")
     else:
-        print("UHH")
         print(message)
     time.sleep(POLL_INTERVAL)
-
-def snmp_works_question_mark(pipe):
-    ping_failures = 0
-    snmp_failures = 0
-
-    while True:
-        if not can_ping(SNMP_TARGET):
-            ping_failures += 1
-            print(f"Can't ping {SNMP_TARGET}. Attempt {ping_failures}/5.")
-            if ping_failures >= 5:
-                print(f"Failed to ping {SNMP_TARGET} 5 times consecutively. Sending error.")
-                handle_error(pipe, "UHH due to ping failure")
-                # Resetting the counter so we can continue monitoring after sending the error.
-                ping_failures = 0
-            time.sleep(POLL_INTERVAL)
-        else:
-            ping_failures = 0  # Reset the counter if ping is successful.
-
-        if not can_snmp(SNMP_TARGET, SNMP_V2_COMMUNITY):
-            snmp_failures += 1
-            print(f"Can't get SNMP from {SNMP_TARGET}. Attempt {snmp_failures}/5.")
-            if snmp_failures >= 5:
-                print(f"Failed to get SNMP data from {SNMP_TARGET} 5 times consecutively. Sending error.")
-                handle_error(pipe, "UHH due to SNMP failure")
-                # Resetting the counter so we can continue monitoring after sending the error.
-                snmp_failures = 0
-            time.sleep(POLL_INTERVAL)
-        else:
-            snmp_failures = 0  # Reset the counter if SNMP fetch is successful.
-
 
 def snmp_fetch(pipe):
     while True:
@@ -202,7 +171,7 @@ def snmp_child(pipe=None):
     in_rate = (prev_in - 0) * 8  # Initializing in_rate
     out_rate = (prev_out - 0) * 8  # Initializing out_rate
     total_bps = in_rate + out_rate  # Initialize total_bps
-    consecutive_failures = 0  # Count for consecutive failures
+    data_ready = False
 
     # If it's the first data fetch, we'll send "SNP" for the display
     first_fetch = True
@@ -221,7 +190,7 @@ def snmp_child(pipe=None):
     snmp_thread.start()
 
     while True:
-        #snmp_works_question_mark(pipe)
+        # Check if the snmp_fetch_thread has updated the values
         if prev_values["current_time"] != prev_time:
             actual_interval = prev_values["current_time"] - prev_time
 
@@ -230,23 +199,20 @@ def snmp_child(pipe=None):
                 new_out_rate = (prev_values["current_out"] - prev_out) * 8 / actual_interval
                 new_total_bps = new_in_rate + new_out_rate
             else:
+                # Here you can decide how to handle the case when actual_interval is 0.
+                # For example, you could use the previous values:
                 new_in_rate = (prev_values["current_in"] - prev_in) * 8
                 new_out_rate = (prev_values["current_out"] - prev_out) * 8
                 new_total_bps = new_in_rate + new_out_rate
 
+            # Check if either in_rate or out_rate are zero, or if they're both None.
+            # If any of these conditions are met, we'll keep the previous total_bps.
             if new_in_rate == 0 or new_out_rate == 0 or (new_in_rate is None and new_out_rate is None):
-                consecutive_failures += 1
-                print(f"Consecutive failures: {consecutive_failures}")
-                
-                if consecutive_failures >= 4:
-                    total_bps = "UHH"
-                    consecutive_failures = 0
+                total_bps = total_bps  # essentially does nothing, but makes it clear in the code
             else:
                 in_rate = new_in_rate
                 out_rate = new_out_rate
                 total_bps = new_total_bps
-                consecutive_failures = 0  # Reset the consecutive failures
-
             if first_fetch:
                 formatted_total = "SNP"
                 first_fetch = False
@@ -269,26 +235,18 @@ def snmp_child(pipe=None):
             prev_out = prev_values["current_out"]
             prev_time = prev_values["current_time"]
 
-        if not first_fetch and total_bps != "UHH":  # Only send fuzzed values if total_bps is not "UHH"
-            for _ in range(int(POLL_INTERVAL * 10)):
-                time.sleep(0.1)
+        # Send fuzzed values ONLY if first_fetch is False
+        if not first_fetch:
+            for _ in range(int(POLL_INTERVAL * 10)):  # 10 fuzzed values every second for the entire POLL_INTERVAL
+                time.sleep(0.1)  # Update every 100ms
                 fuzzed_bps = get_fuzzed_value(total_bps)
-
+                # Check if fuzzed_bps is zero, make it the last non-zero value
                 if fuzzed_bps == "0":
                     fuzzed_bps = last_non_zero_fuzzed_bps
                 else:
                     last_non_zero_fuzzed_bps = fuzzed_bps
 
                 formatted_fuzzed_total = format_bps(fuzzed_bps)
-
-
-                # Print fuzzed value only once every 5 seconds
-                fuzzed_print_counter += 1
-                if fuzzed_print_counter >= 50:  # With the sleep of 0.1s, this will be roughly every 5 seconds
-                    if not pipe:
-                        print(data_to_send_fuzzed['debug'])
-                    fuzzed_print_counter = 0
-
 
                 data_to_send_fuzzed = {
                     'data': formatted_fuzzed_total,
@@ -299,7 +257,6 @@ def snmp_child(pipe=None):
                     pipe.send(data_to_send_fuzzed)
                 else:
                     print(data_to_send_fuzzed['debug'])
-
 
 if __name__ == '__main__':
     snmp_child()
